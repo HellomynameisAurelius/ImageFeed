f0a73156495b19660adc2604439977f6d9c3d02a
HellomynameisAurelius <hellomynameisaurelius@gmail.com>
2023-05-09 16:15:01 +0300

    1) Added LaunchScreen 2) Added UIColor+Extensions

---
+++ ImageFeed/Helpers/UIColor+Extensions.swift
@@ -0,0 +1,18 @@
+//
+//  UIColor+Extensions.swift
+//  ImageFeed
+//
+//  Created by Valeriy on 09.05.2023.
+//
+
+import UIKit
+
+extension UIColor {
+    static var ypBlack: UIColor { UIColor(named: "YP Black") ?? UIColor.black }
+    static var ypGray: UIColor { UIColor(named: "YP Gray") ?? UIColor.gray }
+    static var ypBackground: UIColor { UIColor(named: "YP Background") ?? UIColor.darkGray }
+    static var ypWhite: UIColor { UIColor(named: "YP White") ?? UIColor.white }
+    static var ypWhiteAlpha50: UIColor { UIColor(named: "YP White (Alpha 50)") ?? UIColor.white.withAlphaComponent(0.5)}
+    static var ypRed: UIColor { UIColor(named: "YP Red") ?? UIColor.red }
+    static var ypBlue: UIColor { UIColor(named: "YP Blue") ?? UIColor.blue }
+}
