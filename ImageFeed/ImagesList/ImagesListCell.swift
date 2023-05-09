bbaf64b3c54c404cb987feaee743b4c9ab1960c8
HellomynameisAurelius <hellomynameisaurelius@gmail.com>
2023-05-09 20:14:54 +0300

    1) Added UITableView which contents 20 images in its reusable cell 2) Images has date label and like button. On every odd image like is not active, on every even it is 3) Cells height depends on the size of an image and is dynamic

---
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -0,0 +1,15 @@
+//
+//  ImagesListCell.swift
+//  ImageFeed
+//
+//  Created by Valeriy on 09.05.2023.
+//
+
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    static let reuseIdentifier = "ImagesListCell"
+    @IBOutlet weak var cellImage: UIImageView!
+    @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet weak var dateLabel: UILabel!
+}
