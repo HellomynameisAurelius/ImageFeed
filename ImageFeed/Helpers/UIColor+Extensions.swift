c9d3fc6e10eddfe1d1505e37bc1703da12e792fd
HellomynameisAurelius <hellomynameisaurelius@gmail.com>
2023-05-11 08:00:19 +0300

    1) Fixed constraints 2) Fixed assets naming and spacing

--- ImageFeed/Helpers/UIColor+Extensions.swift
+++ ImageFeed/Helpers/UIColor+Extensions.swift
@@ -8,11 +8,11 @@
 import UIKit
 
 extension UIColor {
-    static var ypBlack: UIColor { UIColor(named: "YP Black") ?? UIColor.black }
-    static var ypGray: UIColor { UIColor(named: "YP Gray") ?? UIColor.gray }
-    static var ypBackground: UIColor { UIColor(named: "YP Background") ?? UIColor.darkGray }
-    static var ypWhite: UIColor { UIColor(named: "YP White") ?? UIColor.white }
-    static var ypWhiteAlpha50: UIColor { UIColor(named: "YP White (Alpha 50)") ?? UIColor.white.withAlphaComponent(0.5)}
-    static var ypRed: UIColor { UIColor(named: "YP Red") ?? UIColor.red }
-    static var ypBlue: UIColor { UIColor(named: "YP Blue") ?? UIColor.blue }
+    static var ypBlack: UIColor { UIColor(named: "ypBlack") ?? .black }
+    static var ypGray: UIColor { UIColor(named: "ypGray") ?? .gray }
+    static var ypBackground: UIColor { UIColor(named: "ypBackground") ?? .darkGray }
+    static var ypWhite: UIColor { UIColor(named: "ypWhite") ?? .white }
+    static var ypWhiteAlpha50: UIColor { UIColor(named: "ypWhite(Alpha50)") ?? .white.withAlphaComponent(0.5) }
+    static var ypRed: UIColor { UIColor(named: "ypRed") ?? .red }
+    static var ypBlue: UIColor { UIColor(named: "ypBlue") ?? .blue }
 }
