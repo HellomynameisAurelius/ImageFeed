bbaab457015998f6c3bba9e13a40c1a173e996d3
HellomynameisAurelius <hellomynameisaurelius@gmail.com>
2023-05-12 22:46:42 +0300

    1) Added ProfileViewController

---
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Valeriy on 12.05.2023.
+//
+
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
+    @IBOutlet private var avatarImageView: UIImageView!
+    @IBOutlet private var nameLabel: UILabel!
+    @IBOutlet private var loginNameLabel: UILabel!
+    @IBOutlet private var descriptionLabel: UILabel!
+    
+    @IBOutlet private var logoutButton: UIButton!
+    
+    
+    @IBAction private func didTapLogoutButton() {
+}
+}
