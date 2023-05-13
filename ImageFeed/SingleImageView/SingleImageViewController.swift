f05205adc8826803e409ea25cc3ec752ec19ae04
HellomynameisAurelius <hellomynameisaurelius@gmail.com>
2023-05-13 15:26:27 +0300

    Added SingleImageViewController

---
+++ ImageFeed/SingleImageView/SingleImageViewController.swift
@@ -0,0 +1,12 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by Valeriy on 13.05.2023.
+//
+
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+}
