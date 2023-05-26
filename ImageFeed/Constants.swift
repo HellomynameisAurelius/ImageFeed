212853ccf0f6beba92eae5558fcee9de0e8d205e
HellomynameisAurelius <hellomynameisaurelius@gmail.com>
2023-05-26 05:55:08 +0300

    Added Constants.swift

---
+++ ImageFeed/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  ImageFeed
+//
+//  Created by Valeriy on 26.05.2023.
+//
+
+import Foundation
+
+let AccessKey = "KYF0HxVYzBgj_KiHujz4v5oZyyfTvEf9xZ6zkLc2icQ"
+let SecretKey = "N_K1lsA_WbrZJJSjdUlhvRKu0QeEZN86pCeiFoAAt-o"
+let RedirectURI = "urn:ietf:wg:oauth:2.0:oob"
+let AccessScope = "public+read_user+write_likes"
+let DefaultBaseURL = URL(string: "https://api.unsplash.com")!
